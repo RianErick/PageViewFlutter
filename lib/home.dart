cbc1ab720ecf4b2a825597c92391a069a0d0778c
RianErick <rianzx34@gmail.com>
2022-06-04 18:54:53 -0300

    Drawer Adicionado

--- lib/home.dart
+++ lib/home.dart
@@ -19,17 +19,63 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: Text("BLOCO DE NOTAS"),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Rian Erick"),
+              accountEmail: Text("riznzx@gmail.com"),
+              currentAccountPicture: CircleAvatar(child: Text("R")),
+            ),
+            ListTile(
+              title: Text("Item 1"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                _pageController.jumpToPage(0);
+                Navigator.pop(context);
+                setState(() {
+                  indexBottomNaviBar = 0;
+                });
+              },
+            ),
+            ListTile(
+              title: Text("Item 2"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                _pageController.jumpToPage(1);
+                Navigator.pop(context);
+                setState(() {
+                  indexBottomNaviBar = 0;
+                });
+              },
+            ),
+            ListTile(
+              title: Text("Item 3"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                _pageController.jumpToPage(2);
+                Navigator.pop(context);
+                setState(
+                  () {
+                    indexBottomNaviBar = 2;
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: PageView(controller: _pageController, children: [
         Container(
           color: Colors.white,
           child: Text("Imagens"),
         ),
         Container(
-          color: Colors.green,
+          color: Colors.white,
           child: Text("Anotações"),
         ),
         Container(
-          color: Colors.amber,
+          color: Colors.white,
           child: Text("Textos"),
         )
       ]),
@@ -48,7 +94,7 @@ class _HomePageState extends State<HomePage> {
             BottomNavigationBarItem(
                 icon: Icon(Icons.notes), label: "Anotações"),
             BottomNavigationBarItem(
-                icon: Icon(Icons.text_decrease), label: "Textos"),
+                icon: Icon(Icons.text_fields_outlined), label: "Textos"),
           ]),
     );
   }
